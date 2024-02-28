ee4fa7883618f6d9ecf41719688dccdff2ace0d8
chotaki <anaelle.romanzin@gmail.com>
2024-02-28 12:01:33 +0100

    link engine and game & display in the game the window generated in the engine

--- GameEngine/transform.h
+++ GameEngine/transform.h
@@ -1,36 +1,36 @@
-#pragma once
-
-#include <d3d12.h>
-#include <DirectXMath.h>
-#include <DirectXPackedVector.h>
-
-struct Transform
-{
-    DirectX::XMFLOAT3 vSca;
-    DirectX::XMMATRIX mSca;
-
-    DirectX::XMFLOAT3 vDir;
-    DirectX::XMFLOAT3 vRight;
-    DirectX::XMFLOAT3 vUp;
-    DirectX::XMFLOAT4 qRot;
-    DirectX::XMMATRIX mRot;
-
-    DirectX::XMFLOAT3 vPos;
-    DirectX::XMMATRIX mPos;
-
-    DirectX::XMMATRIX matrix;
-};
-void Identity();
-void FromMatrix(DirectX::XMMATRIX* pMat);
-void UpdateRotationFromVectors();
-void UpdateRotationFromQuaternion();
-void UpdateRotationFromMatrix();
-void UpdateMatrix();
-void Rotate(float yaw, float pitch, float roll);
-void RotateYaw(float angle);
-void RotatePitch(float angle);
-void RotateRoll(float angle);
-void RotateWorld(DirectX::XMMATRIX* pMatrix);
-void RotateWorldX(float angle);
-void RotateWorldY(float angle);
-void RotateWorldZ(float angle);
+//#pragma once
+//
+//#include <d3d12.h>
+//#include <DirectXMath.h>
+//#include <DirectXPackedVector.h>
+//
+//struct Transform
+//{
+//    DirectX::XMFLOAT3 vSca;
+//    DirectX::XMMATRIX mSca;
+//
+//    DirectX::XMFLOAT3 vDir;
+//    DirectX::XMFLOAT3 vRight;
+//    DirectX::XMFLOAT3 vUp;
+//    DirectX::XMFLOAT4 qRot;
+//    DirectX::XMMATRIX mRot;
+//
+//    DirectX::XMFLOAT3 vPos;
+//    DirectX::XMMATRIX mPos;
+//
+//    DirectX::XMMATRIX matrix;
+//};
+//void Identity();
+//void FromMatrix(DirectX::XMMATRIX* pMat);
+//void UpdateRotationFromVectors();
+//void UpdateRotationFromQuaternion();
+//void UpdateRotationFromMatrix();
+//void UpdateMatrix();
+//void Rotate(float yaw, float pitch, float roll);
+//void RotateYaw(float angle);
+//void RotatePitch(float angle);
+//void RotateRoll(float angle);
+//void RotateWorld(DirectX::XMMATRIX* pMatrix);
+//void RotateWorldX(float angle);
+//void RotateWorldY(float angle);
+//void RotateWorldZ(float angle);
