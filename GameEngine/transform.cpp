366cfe04686378d6106af5532df57bee28a408d6
ederveaux <ederveaux@GCFRLY260>
2024-02-28 12:02:44 +0100

    continue transform

--- GameEngine/transform.cpp
+++ GameEngine/transform.cpp
@@ -1,28 +1,53 @@
 #include "Transform.h"
-// Créer un quaternion pour chaque rotation (delta)
-DirectX::XMFLOAT4 quat;
-D3DXQuaternionRotationAxis(&quat, &m_vDir, roll);
-quatRot = quat;
-D3DXQuaternionRotationAxis(&quat, &m_vRight, pitch);
-quatRot *= quat;
-D3DXQuaternionRotationAxis(&quat, &m_vUp, yaw);
-quatRot *= quat;
+void Transform::Rotate() {
+	// Créer un quaternion pour chaque rotation (delta)
+	DirectX::XMVECTOR quat;
+	const DirectX::XMFLOAT3 vDirConst = vDir;
+	const DirectX::XMFLOAT3 vRightConst = vRight;
+	const DirectX::XMFLOAT3 vUpConst = vUp;
+	DirectX::XMVECTOR vDirVect = DirectX::XMLoadFloat3(&vDirConst);
+	DirectX::XMVECTOR vRightVect = DirectX::XMLoadFloat3(&vRightConst);
+	DirectX::XMVECTOR vUpVect = DirectX::XMLoadFloat3(&vUpConst);
+	DirectX::XMVector3Rotate(vDirVect, quat);
+	DirectX::XMVECTOR quatRot = quat;
+	DirectX::XMVector3Rotate(vRightVect, quat);
+	quatRot = DirectX::XMVector3Cross(quatRot, quat);
+	DirectX::XMVector3Rotate(vUpVect, quat);
+	quatRot = DirectX::XMVector3Cross(quatRot, quat);
 
-// Ajouter la rotation delta à la rotation actuelle de l’objet
-m_quatRot *= quatRot;
+	// Ajouter la rotation delta à la rotation actuelle de l’objet
+	qRot = DirectX::XMVector3Cross(qRot, quatRot);
 
-// Convertir le quaternion en une matrice de rotation (magique)
-D3DXMATRIX matRot;
-D3DXMatrixRotationQuaternion(&matRot, &m_quatRot);
+	// Convertir le quaternion en une matrice de rotation (magique)
+	DirectX::XMMATRIX matRot;
+	matRot = DirectX::XMMatrixRotationQuaternion(qRot);
 
-// Mettre à jour les axes de notre objet (3 vecteurs)
-m_vRight.x = matRot._11;
-m_vRight.y = matRot._12;
-m_vRight.z = matRot._13;
-m_vUp.x = matRot._21;
-m_vUp.y = matRot._22;
-m_vUp.z = matRot._23;
-m_vDir.x = matRot._31;
-m_vDir.y = matRot._32;
-m_vDir.z = matRot._33;
+	// Mettre à jour les axes de notre objet (3 vecteurs)
+	DirectX::XMFLOAT4X4 matRotFloat; 
+	DirectX::XMStoreFloat4x4(&matRotFloat, matRot);
+	vRight.x = matRotFloat._11;
+	vRight.y = matRotFloat._12;
+	vRight.z = matRotFloat._13;
+	vUp.x = matRotFloat._21;
+	vUp.y = matRotFloat._22;
+	vUp.z = matRotFloat._23;
+	vDir.x = matRotFloat._31;
+	vDir.y = matRotFloat._32;
+	vDir.z = matRotFloat._33;
+}
 
+
+// Toujours initialiser les matrices, les quaternions, les vecteurs…
+mSca = DirectX::XMMatrixIdentity();
+mPos = DirectX::XMMatrixIdentity();
+mRot = DirectX::XMMatrixIdentity();
+
+// On peut effectuer des operations sur chaque matrice indépendamment
+mSca = DirectX::XMMatrixScaling(x, y, z);
+mPos = DirectX::XMMatrixTranslation(x, y, z);
+mRot = DirectX::XMMatrixRotationY(3.14f);
+
+// Combiner les matrices dans cet ordre
+DirectX::XMMATRIX mat = mSca;
+mat *= mRot;
+mat *= mPos;
--- GameEngine/transform.h
+++ GameEngine/transform.h
@@ -12,25 +12,18 @@ struct Transform
     DirectX::XMFLOAT3 vDir;
     DirectX::XMFLOAT3 vRight;
     DirectX::XMFLOAT3 vUp;
-    DirectX::XMFLOAT4 qRot;
+    DirectX::XMVECTOR qRot;
     DirectX::XMMATRIX mRot;
 
     DirectX::XMFLOAT3 vPos;
     DirectX::XMMATRIX mPos;
 
     DirectX::XMMATRIX matrix;
+    void FromMatrix(DirectX::XMMATRIX* pMat);
+    void UpdateRotationFromVectors();
+    void UpdateRotationFromQuaternion();
+    void UpdateRotationFromMatrix();
+    void UpdateMatrix();
+    void Rotate();
+    void RotateWorld(DirectX::XMMATRIX* pMatrix);
 };
-void Identity();
-void FromMatrix(DirectX::XMMATRIX* pMat);
-void UpdateRotationFromVectors();
-void UpdateRotationFromQuaternion();
-void UpdateRotationFromMatrix();
-void UpdateMatrix();
-void Rotate(float yaw, float pitch, float roll);
-void RotateYaw(float angle);
-void RotatePitch(float angle);
-void RotateRoll(float angle);
-void RotateWorld(DirectX::XMMATRIX* pMatrix);
-void RotateWorldX(float angle);
-void RotateWorldY(float angle);
-void RotateWorldZ(float angle);
