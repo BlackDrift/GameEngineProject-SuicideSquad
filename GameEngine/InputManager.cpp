9ac4c867f527de933f126f33d875734a5ff76f2e
ederveaux <ederveaux@GCFRLY260>
2024-03-01 09:27:27 +0100

    inputmanager repush

--- GameEngine/InputManager.cpp
+++ GameEngine/InputManager.cpp
@@ -1,12 +1,12 @@
 #include "InputManager.h"
-InputManager::InputManager(int key, bool type) {
+InputManager::InputManager(int key, char* type) {
 	this->key = key;
 	this->type = type;
 }
-char* InputManager::Update() {
+bool InputManager::Update() {
 	lastInput = input;
 	if (type == "keyboard") {
-		input = GetAsyncKeyState(key)
+		input = GetAsyncKeyState(key);
 	}
 	return input;
 }
