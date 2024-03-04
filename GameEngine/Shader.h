e7149f983b7eac2b9f248cc7ad7e08557485d1c6
ederveaux <ederveaux@GCFRLY260>
2024-03-04 19:59:28 +0100

    ft: Texture

--- GameEngine/Shader.h
+++ GameEngine/Shader.h
@@ -1,19 +1,4 @@
 #pragma once
-
-#include <d3d12.h>
-#include <DirectXMath.h>
-#include <wrl.h>
-#include <iostream>
 class Shader
 {
-public:
-	Shader(Microsoft::WRL::ComPtr<ID3D12Device> device, DXGI_FORMAT bBufferFormat, DXGI_FORMAT dStencilFormat, bool MSAAState, UINT MSSAAQuality);
-	~Shader();
-
-	/*ComPtr<ID3DBlob> mvsByteCode;
-	ComPtr<ID3DBlob> mpsByteCode;
-	std::vector<D3D12_INPUT_ELEMENT_DESC> mInputLayout;
-
-	ComPtr<ID3D12PipelineState> mPSO = nullptr;*/
-
 };
--- GameEngine/Texture.cpp
+++ GameEngine/Texture.cpp
@@ -1,1 +1,5 @@
 #include "Texture.h"
+#include "DDSTextureLoader.h"
+Texture::Texture(char* file) {
+
+}
