f98494701b4882c4d4a6744e86b9b1f0c135bea3
ederveaux <ederveaux@GCFRLY260>
2024-03-04 18:54:43 +0100

    continue GameObject

--- GameEngine/GameObject.cpp
+++ GameEngine/GameObject.cpp
@@ -1,7 +1,9 @@
 #include "GameObject.h"
-GameObject::GameObject(DirectX::XMVECTOR pos, DirectX::XMVECTOR forward = { 0, 0, -1 }, float width, float height) {
+GameObject::GameObject(DirectX::XMVECTOR pos, DirectX::XMVECTOR forward = { 0, 0, -1 }, float width, float height, std::vector<Component> listComponents, Texture texture) {
 	this->pos = pos;
 	this->forward = forward;
 	this->width = width;
 	this->height = height;
+	this->listComponents = listComponents;
+	this->texture = texture;
 }
--- GameEngine/GameObject.h
+++ GameEngine/GameObject.h
@@ -1,12 +1,17 @@
 #pragma once
 #include "Transform.h"
+#include "Component.h"
+#include "Texture.h"
+#include <iostream>
+#include <vector>
 class GameObject
 {
-	GameObject(DirectX::XMVECTOR pos, DirectX::XMVECTOR forward = { 0, 0, -1 }, float width, float height);
+	GameObject(DirectX::XMVECTOR pos, DirectX::XMVECTOR forward = { 0, 0, -1 }, float width, float height, std::vector<Component> listComponents, Texture texture);
 	Transform transform;
 	DirectX::XMVECTOR pos;
 	DirectX::XMVECTOR forward;
 	float width;
 	float height;
+	std::vector<Component> listComponents;
+	Texture texture
 };
-
